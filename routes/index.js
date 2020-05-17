53e97516a08299451abff69adaea57457ae1f19d
okezino <okezi002@gmail.com>
2020-05-17 17:51:35 +0100

    third commit

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
 
 module.exports = router;
